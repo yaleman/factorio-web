bacef76f70bf04c33af5c62aecbe50c31c67f677
James Hodgkinson <james@terminaloutcomes.com>
2025-11-13 12:42:09 +1000

    adding tests and fixing linting etc

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,6 +1,17 @@
-from factorio_web import app
+from factorio_web import app, static_file
+from litestar.testing import TestClient
 
 
 def test_app_dir() -> None:
     """terrible test that basically just checks that the app object exists"""
     assert "route_map" in dir(app)
+
+
+def test_static_file() -> None:
+    """test static file handler with a known file"""
+
+    client = TestClient(app)
+    response = client.get("/static/script.js")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "application/javascript"
+    assert "function" in response.text  # crude check that it's JS content
